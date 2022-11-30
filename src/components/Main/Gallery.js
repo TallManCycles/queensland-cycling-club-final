270e50d6da5262e1e3f3ae039c7dfff2c7cf192b
TallManCycles <Aaronday@live.com.au>
2022-11-30 21:52:42 +1000

    deploy gh-pages

--- src/components/Main/Gallery.js
+++ src/components/Main/Gallery.js
@@ -22,25 +22,25 @@ export default class Gallery extends Component {
     const images = [
         {   
             id: 1,
-            src: "../../img/crosscountry.jpg", 
+            src: "/img/crosscountry.jpg", 
             alt: "man riding a cross country bike up a hill", 
             caption: "Cross Country Regional Finals"},
 
         {
             id: 2,
-            src: "../../img/longevent.jpg", 
+            src: "/img/longevent.jpg", 
             alt: "two cyclists under trees on a road", 
             caption: "Open Road Classic"},
 
         {
             id: 3,
-            src: "../../img/moutain bike.jpg", 
+            src: "/img/moutain bike.jpg", 
             alt: "mountain bike in the bushland", 
             caption: "Open Mountain Bike Finals"},
 
         {
             id: 4,
-            src: "../../img/roadrace.jpg", 
+            src: "/img/roadrace.jpg", 
             alt: "cyclists handlebars at the start of a race", 
             caption: "Mens Open Crit Race"}
     ]
@@ -52,18 +52,12 @@ export default class Gallery extends Component {
             <h2>Recent Races</h2>
                 <section className="photoGallery">
                     {images.map( image => (
-                            <div key={image.id}>
-                            <figure>
-                            <img src={image.src} alt={image.alt}/>
-                            <figcaption>{image.caption}</figcaption>
-                            </figure>
-                        </div>
-                        // <Figure
-                        //     key={image.id} 
-                        //     src={imageSource}
-                        //     alt={image.alt}
-                        //     caption={image.caption} 
-                        // />
+                        <Figure
+                            key={image.id} 
+                            src={image.src}
+                            alt={image.alt}
+                            caption={image.caption} 
+                        />
                     ))}
                 </section>
             </div>
