3da3e867573c29d73c438a6c4607917b8f663658
TallManCycles <Aaronday@live.com.au>
2022-12-06 13:04:01 +1000

    Added tests

--- src/App.test.js
+++ src/App.test.js
@@ -5,29 +5,37 @@ import About from './components/Aside/About';
 import RecentRaces from './components/Main/Gallery';
 import Weather from "./components/Main/Weather";
 
-test('renders header test', () => {
+test('Header is rendered', () => {
   render(<App />);
   const linkElement = screen.getByText("Queensland Cycling Club");
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders upcoming races test', () => {
+test('Upcoming races is visible', () => {
   render(<Upcomingraces />);
   const linkElement = screen.getByText("Upcoming Races");
   expect(linkElement).toBeInTheDocument();
 });
 
-test("test button text", () => {
+test("Contact us button is visible", () => {
   render(<About />);
   const contactButton = screen.getByText(/Contact Us/i);
   expect(contactButton).toHaveTextContent(/Contact Us/i);
 });
 
-test("test image view", () => {
+test("Alt image text exists", () => {
   render(<RecentRaces />);
   const image = screen.getByAltText(/man riding a cross country bike up a hill/i);
   expect(image).toBeInTheDocument();
 });
+
+
+test("Weather information viewable", () => {
+  render(<Weather />);
+  const minimumTemp = screen.getByAltText(/Min Temp (C°)/i);
+  expect(minimumTemp).toBeInTheDocument();
+});
+
 
 
 
